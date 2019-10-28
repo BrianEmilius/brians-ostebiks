5a1dc2acf8be6740cf985756fe114ee3b87a22ef
Brian Emilius <brian@brianemilius.com>
2019-10-28 20:47:11 +0100

    add cors stuffs and stuff

--- src/javascript/get-product.js
+++ src/javascript/get-product.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
 		.then(response => response.json())
 		.then(function(product) {
 			const container = document.getElementsByClassName("page")[0];
-			container.querySelector("h1").innerText = product.name;
-			container.querySelectorAll("p")[0].innerText = product.description;
-			container.querySelector(".price").innerText = product.price;
-			container.querySelector(".weight").innerText = product.weight;
+			container.querySelector("h1").innerText = product[0].name;
+			container.querySelectorAll("p")[0].innerText = product[0].description;
+			container.querySelector(".price").innerText = product[0].price;
+			container.querySelector(".weight").innerText = product[0].weight;
 		});
 });
